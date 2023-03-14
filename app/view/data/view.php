0c4d7fd6e8a64d5ad57f3eac204c3dd767c7d9e8
Antonio Mauricio <antonioimportant@gmail.com>
2023-03-14 10:12:45 -0300

    view implemented

--- app/view/data/view.php
+++ app/view/data/view.php
@@ -1,2 +1,21 @@
 <?php
     // aqui vai ficar visualização dos dados selecionados na tela anterior
+
+    $conexao = new mysqli("localhost", "root", "", "jornal_senai");
+    $id = $_GET["id"];
+
+
+
+    $query = "SELECT * FROM day WHERE id=$id";
+    $result = mysqli_query($conexao, $query);
+    $exibicao = mysqli_fetch_assoc($result);
+
+?>
+<html>
+    <head>
+        <title><?= $exibicao['data'] ?></title>
+    </head>
+    <body>
+        <?= $exibicao['aviso'] ?>
+    </body>
+</html>
